36cf4672bd9b3b5a9a63b0ba2c29882d630b8f96
ahmad75reza <ahmad75reza>
2022-04-14 00:59:08 +0530

    some more work

--- java-code/main.java
+++ java-code/main.java
@@ -1,6 +1,8 @@
 public class main {
-	public static void main(String[] a) {
+	public static void main(String[] aa) {
 		System.out.print("Hello World\n");
 		System.out.print("next line\n");
+		int a = 40;
+		System.out.print(a);
 	}
 }
